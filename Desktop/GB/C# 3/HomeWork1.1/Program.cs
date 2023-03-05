c6cacf8d85b4cf53f5fcf23f7e25f05dd9a5535c
Anna <lianna2015new@yandex.ru>
2023-03-05 13:03:59 +1000

    added program found polindrom

---
+++ Desktop/GB/C# 3/HomeWork1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.Write("Введите пятизначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstNumber = number/10000;
+int lastNumber = number % 10;
+int secondNumber = (number / 1000)%10;
+int fourNumber = (number / 10)%10;
+
+if (firstNumber == lastNumber && secondNumber == fourNumber )
+{
+    Console.WriteLine ($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine ($"Число {number} не является палиндромом");
+}
+
+
+
+
+
