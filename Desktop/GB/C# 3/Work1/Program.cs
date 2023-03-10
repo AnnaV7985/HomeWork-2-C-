66e8c4bd22d6329f8c5ca8f6d2dc10390a2af752
Anna <lianna2015new@yandex.ru>
2023-03-10 11:17:50 +1000

    added found nummber of digits

---
+++ Desktop/GB/C# 3/Work1/Program.cs
@@ -0,0 +1,28 @@
+﻿// сумма чисел от 1 до А
+/*Console.Write("Введите  число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum =0; 
+
+for (int i = 1; i < N; i++) sum= sum + i;
+{
+    N = N + sum;
+    Console.WriteLine($"Сумма чисел = {N}:");
+}
+
+
+2. определить кол-во цифр в числе:
+  
+*/
+
+Console.Write("Введите  число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (N > 1)
+{
+    N = N%10;
+    
+    N = N/10; 
+   count++ ;
+   
+}
+ Console.WriteLine(count);
