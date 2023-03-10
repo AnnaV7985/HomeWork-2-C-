d184651ed1c44f7f26196cd5c5efe7e589c841dc
Anna <lianna2015new@yandex.ru>
2023-03-10 16:59:43 +1000

    added found count of  even nummber

---
+++ Desktop/GB/C#4/FunctionHW-1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество 
+//чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] resArray = NewArray(10, 100, 999);
+Console.WriteLine($"массив: [{String.Join("," , resArray)}]");
+
+int[] NewArray (int size, int min, int max)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min,max); 
+    }
+    return array;
+}
+
+int evenNumber=0;
+int countFind =0;
+for (int i = 0; i < resArray.Length; i++)
+{
+    if (resArray[i]%2 == 0)
+    {
+        evenNumber = resArray[i];
+        countFind ++ ;
+    }
+}
+Console.WriteLine($"Четных чисел в массиве {countFind}");
