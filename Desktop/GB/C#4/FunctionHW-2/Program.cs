e1c56c0de7701a05de7c7559ada48a80e59fb123
Anna <lianna2015new@yandex.ru>
2023-03-11 15:17:21 +1000

    added found sum of odd  nummber

---
+++ Desktop/GB/C#4/FunctionHW-2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] NewArray (int size, int min, int max)
+{
+    int[] arr = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min,max);
+    }
+    return arr;
+}
+int[] resArray = NewArray (5, 1, 10);
+Console.WriteLine ($"В массиве: [{String.Join(",", resArray)}]");
+int oddNumber = 0;
+for (int i = 0; i < resArray.Length; i++)
+{
+    if (resArray[i] % 2 != 0) 
+    {
+       oddNumber += resArray[i];
+    }
+}
+Console.WriteLine ($"Сумма нечетных чисел: {oddNumber}");
