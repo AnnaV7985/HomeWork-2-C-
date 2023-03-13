5b6372c2b179d181d5bb77542be41718be82301c
Anna <lianna2015new@yandex.ru>
2023-03-13 16:03:59 +1000

    found sum of digit

--- Desktop/GB/C#3.1/Program.cs
+++ Desktop/GB/C#3.1/Program.cs
@@ -1,12 +1,13 @@
-﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// 3, 5 -> 243 (3⁵)
+// // 3, 5 -> 243 (3⁵)
 
-// 2, 4 -> 16
+// // 2, 4 -> 16
 
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(Math.Pow (firstNumber,secondNumber));
+// Console.Write(Math.Pow (firstNumber,secondNumber));
+
