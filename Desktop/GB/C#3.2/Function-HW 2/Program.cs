ce5afb1ecb157672930112003ced0ca107969205
Anna <lianna2015new@yandex.ru>
2023-03-13 16:41:31 +1000

    add array

---
+++ Desktop/GB/C#3.2/Function-HW 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int [] elementArray = NewArray (3, 1,20);
+// int[] NewArray (int size, int min, int max)
+// {
+// int[] array = new int [size];
+// for (int i = 0; i < size; i++)
+// {
+    
+// array [i] = new Random().Next(min,max);
+// }
+// return array;
+
+// }
+//     Console.WriteLine ($"[{String.Join(",", elementArray)}]");
+
+
+
