511f37545aed5b84d339f51c2e8ea684bd94df5b
Anna <lianna2015new@yandex.ru>
2023-03-14 17:10:29 +1000

    add point of intersection

--- Desktop/GB/C#3.1/Program.cs
+++ Desktop/GB/C#3.1/Program.cs
@@ -4,10 +4,10 @@
 
 // // 2, 4 -> 16
 
-// Console.Write("Введите первое число: ");
-// int firstNumber = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите второе число: ");
-// int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write(Math.Pow (firstNumber,secondNumber));
+Console.Write(Math.Pow (firstNumber,secondNumber));
 
